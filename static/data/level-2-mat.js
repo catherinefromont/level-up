49929cc82d6ee3064968275a95dcf6af36ead759
Your Name <catherinegfromont@gmail.com>
2018-02-05 10:39:25 +1300

    added routes.js

---
+++ static/data/level-2-mat.js
@@ -0,0 +1,39 @@
+const level2Mat = [
+  {
+    id: 1,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 2,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 3,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 4,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 5,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  }
+]
+
+export default level2Mat
---
+++ static/data/level-3-mat.js
@@ -0,0 +1,39 @@
+const level3Mat = [
+  {
+    id: 1,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 2,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 3,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 4,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  },
+  {
+    id: 5,
+    question: '',
+    selection: ['', '', '', ''],
+    answer: '',
+    description: ''
+  }
+]
+
+export default level3Mat
