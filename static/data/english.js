65c450002f1971dbc9ae0cff342bb9d4de4a4a12
Your Name <catherinegfromont@gmail.com>
2018-02-07 21:38:37 +1300

    restructured data files

---
+++ static/data/english.js
@@ -0,0 +1,115 @@
+const english = {
+  level1Eng: [
+    {
+      id: 1,
+      question: 'What is a colloquial?',
+      selection: ['Informal spoken language', 'An exaggeration used for effect', 'An imitation of a sound they represent', 'A comparison of two things'],
+      answer: 'Informal spoken language',
+      description: 'Informal spoken language is used to give a conversational tone.'
+    },
+    {
+      id: 2,
+      question: 'What is an example of a Personification?',
+      selection: ['"Traffic slowed to a crawl."', '"The sun is bright and yellow."', '"The door went SLAM!"', '"The snow is a white blanket."'],
+      answer: '"Traffic slowed to a crawl."',
+      description: 'A Personification is the attribution of a personal nature or human characteristics to something non-human, or the representation of an abstract quality in human form.'
+    },
+    {
+      id: 3,
+      question: 'What is a Euphemism?',
+      selection: ['A way of emphasising a point.', 'A phrase that is less offensive or less troubling than another.', 'Language that makes fun of someone.', 'A direct request or command.'],
+      answer: 'A phrase that is less offensive or less troubling than another.',
+      description: 'A Euphemism is a mild or indirect word or expression substituted for one considered to be too harsh or blunt when referring to something unpleasant or embarrassing.'
+    },
+    {
+      id: 4,
+      question: 'What is third person viewpoint?',
+      selection: ['Uses "I"', 'Uses "he, she"', 'An all seeing, all knowing narrator.', 'Uses "their, them"'],
+      answer: 'Uses "he, she"',
+      description: 'Third Person Viewpoint is when the narrator is not a character, therefore uses "he, she".'
+    },
+    {
+      id: 5,
+      question: 'What is the coreect term for an exaggeration used for effect?',
+      selection: ['Repitition', 'Hyperbole', 'Anecdote', 'Pun'],
+      answer: 'Hyperbole',
+      description: 'A Hyperbole is an exaggerated statement or claim not meant to be taken literally (for effect).'
+    }
+  ],
+  level2Eng: [
+    {
+      id: 1,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 2,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 3,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 4,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 5,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    }
+  ],
+  level3Eng: [
+    {
+      id: 1,
+      question: 'What is an example of a Personification?',
+      selection: ['"Traffic slowed to a crawl."', '"The sun is bright and yellow."', '"The door went SLAM!"', '"The snow is a white blanket."'],
+      answer: '"Traffic slowed to a crawl."',
+      description: 'A Personification is the attribution of a personal nature or human characteristics to something non-human, or the representation of an abstract quality in human form.'
+    },
+    {
+      id: 2,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 3,
+      question: 'What is a Euphemism?',
+      selection: ['A way of emphasising a point.', 'A phrase that is less offensive or less troubling than another.', 'Language that makes fun of someone.', 'A direct request or command.'],
+      answer: 'A phrase that is less offensive or less troubling than another.',
+      description: 'A Euphemism is a mild or indirect word or expression substituted for one considered to be too harsh or blunt when referring to something unpleasant or embarrassing.'
+    },
+    {
+      id: 4,
+      question: '',
+      selection: ['', '', '', ''],
+      answer: '',
+      description: ''
+    },
+    {
+      id: 5,
+      question: 'What is third person viewpoint?',
+      selection: ['Uses "I"', 'Uses "he, she"', 'An all seeing, all knowing narrator.', 'Uses "their, them"'],
+      answer: 'Uses "he, she"',
+      description: 'Third Person Viewpoint is when the narrator is not a character, therefore uses "he, she".'
+    }
+  ]
+}
+
+export default english
