f8ae2cbafb7066970c3ca72fb4d262d8e202cdbc
Your Name <catherinegfromont@gmail.com>
2018-02-13 12:19:18 +1300

    state

--- client/components/Start.jsx
+++ client/components/Start.jsx
@@ -7,11 +7,11 @@ class Start extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      name: [],
-      level: [],
-      subject: []
+      name: '',
+      level: '',
+      subject: ''
     }
-    //this.onChange = this.onChange.bind(this)
+    // this.onChange = this.onChange.bind(this)
   }
 
   // onChange () {
